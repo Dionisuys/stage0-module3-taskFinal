f8ce42fd6fbdf4ff33027df5902f02d4d73e6b33
Dzianis Panasiuk <dzianispanasiuk@gmail.com>
2023-04-29 01:43:14 +0300

    updated 2 classes NumbersPrinter and DigitsSumCalculator

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,12 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
-        String numberString = String.valueOf(number);
-        char first = numberString.charAt(0);
-        char second = numberString.charAt(1);
-        char third = numberString.charAt(2);
-        char fourth = numberString.charAt(3);
-        int result = (int) first + (int) second + (int) third + (int) fourth;
-        System.out.println(result);
+        int sum = 0;
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        System.out.println(sum);
     }
 }
