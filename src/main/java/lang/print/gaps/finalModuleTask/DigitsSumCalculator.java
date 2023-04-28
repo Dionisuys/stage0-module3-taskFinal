485c5158671a838d61241bdaaf1952a67d892ecf
Dzianis Panasiuk <dzianispanasiuk@gmail.com>
2023-04-29 01:35:26 +0300

    FINAL MODULE TASK SOLUTION

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        String numberString = String.valueOf(number);
+        char first = numberString.charAt(0);
+        char second = numberString.charAt(1);
+        char third = numberString.charAt(2);
+        char fourth = numberString.charAt(3);
+        int result = (int) first + (int) second + (int) third + (int) fourth;
+        System.out.println(result);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int onesDigit = number % 10;
+        int tensDigit = (number / 10) % 10;
+        int hundredsDigit = number / 100;
+        int reversedNumber = onesDigit * 100 + tensDigit * 10 + hundredsDigit;
+        System.out.println("Reversed number: " + reversedNumber);
     }
 }
